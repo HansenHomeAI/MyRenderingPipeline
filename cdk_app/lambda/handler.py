ad73a6c37e603b9b66b49e035b0e07f09a966d32
HansenHomeAI <168382461+HansenHomeAI@users.noreply.github.com>
2025-03-20 16:28:15 -0600

    Update handler.py

--- cdk_app/lambda/handler.py
+++ cdk_app/lambda/handler.py
@@ -94,6 +94,11 @@ def handler(event, context):
 
     return {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": "*"
+        },
         "body": json.dumps({
             "message": "SageMaker job started",
             "jobId": job_id,
